0f2e470c1d6abb8c6d8844230aefc6e9faf8522f
Chagui- <apeciro@gmail.com>
2022-03-17 22:05:48 -0300

    another try at fixing the time not set issue

--- ESP32Time.cpp
+++ ESP32Time.cpp
@@ -86,12 +86,29 @@ void ESP32Time::setTime(long epoch, int ms) {
   settimeofday(&tv, NULL);
 }
 
+bool getLocalTimeNew(struct tm *info, uint32_t ms)
+{
+    uint32_t start = millis();
+    time_t now;
+    while ((millis() - start) <= ms)
+    {
+        time(&now);
+        localtime_r(&now, info);
+        if (info->tm_year > (1969 - 1900))
+        {
+            return true;
+        }
+        delay(10);
+    }
+    return false;
+}
+
 /*!
     @brief  get the internal RTC time as a tm struct
 */
 tm ESP32Time::getTimeStruct(){
   struct tm timeinfo;
-  getLocalTime(&timeinfo, 10); // cannot be 0, else call would return false sometimes
+  getLocalTimeNew(&timeinfo, 5000);
   return timeinfo;
 }
 
